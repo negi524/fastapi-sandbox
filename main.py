8054aab138acdd183bc14207e8eed88abea65a1b
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-07-05 22:15:48 +0900

    feat: セットアップ完了

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from fastapi-sandbox!")
+# def main():
+#     print("Hello from fastapi-sandbox!")
+#
+#
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
